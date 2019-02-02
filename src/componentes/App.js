4e39b06d5c58eed30033aab3109f85a64b2279c5
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-02 20:31:43 +0100

    Creación del componente Header y su respectivo CSS

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Header from './Header';
 
 class App extends Component {
     render() {
         return (
             <div className="contenedor">
-                <h1>Desde el componente</h1>
+                <Header />
             </div>
         );
     }
---
+++ src/componentes/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <header className="top">
+            <h1>
+                Cotizador de Seguro de auto
+            </h1>
+        </header>
+    )
+};
+
+export default Header;
